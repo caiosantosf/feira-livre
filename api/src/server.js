8a9fb1d73b9d64467a5cf77d9334e5d4197622b5
Caio Santos <caiosantosfreitas@hotmail.com>
2021-01-29 09:59:36 -0300

    criacao da estrutura inicial

---
+++ api/src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const userRoutes = require('./modules/user/user-routes')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(userRoutes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`API server running on port ${process.env.PORT}!`)
+})
