35766c618f003d9dc58a90b1624d40371ef44478
Caio Santos <caiosantosfreitas@hotmail.com>
2021-02-08 11:18:43 -0300

    backed feira

--- api/src/modules/feira/feira-routes.js
+++ api/src/modules/feira/feira-routes.js
@@ -1,58 +1,31 @@
 const express = require('express')
-const { auth, getMany, post, destroy, getOne, put, emailResetPassword } = require('./user-controller')
+const { getMany, post, destroy, getOne, put } = require('./feira-controller')
 const { routeSecurity : security } = require('../../config/security')
 const validation = require('./user-validation')
 
 const routes = express.Router()
 
-routes.post('/users/login', 
-  validation(['cpf', 'password']), 
-  auth
-)
-
-routes.post('/users/login/admin', 
-  validation(['cpf', 'password']), 
-  auth
-)
-
-routes.post('/users/login/emailresetpassword',
-  validation(['email']), 
-  emailResetPassword
-)
-
-routes.patch('/users/login/resetpassword/:id', 
-  security(['regular', 'admin']), 
-  validation(['password']), 
-  put
-)
-
-routes.get('/users', 
-  security(['admin']), 
+routes.get('/feiras',
   getMany
 )
 
-routes.post('/users', 
-  validation(['name', 'cpf', 'phone', 'email', 'password', 'documentType', 
-              'document', 'cep', 'homeAddress', 'addressNumber',
-              'neighborhood', 'city', 'state', 'birth']), 
+routes.post('/feiras', 
+  validation(['cidade', 'estado', 'descricao']), 
   post
 )
 
-routes.get('/users/:id', 
-  security(['regular', 'admin']), 
+routes.get('/feiras/:id',
   getOne
 )
 
-routes.put('/users/:id', 
-  security(['regular', 'admin']), 
-  validation(['name', 'cpf', 'phone', 'email', 'password', 'documentType', 
-              'document', 'cep', 'homeAddress', 'addressNumber',
-              'neighborhood', 'city', 'state', 'birth']),
+routes.put('/feiras/:id', 
+  security(['feira']), 
+  validation(['cidade', 'estado', 'descricao']),
   put
 )
 
-routes.delete('/users/:id', 
-  security(['regular', 'admin']), 
+routes.delete('/feiras/:id', 
+  security(['feira']), 
   destroy
 )
 
